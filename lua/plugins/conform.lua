208464b1b3d35f82c1e65d6e578ad98573d4421c
Sam de Groot <sammiedegroot@gmail.com>
2024-11-13 21:02:57 +0100

    .

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,36 @@
+return {
+  "stevearc/conform.nvim",
+  event = { "BufWritePre" },
+  cmd = { "ConformInfo" },
+  keys = {
+    {
+      "<leader>f",
+      function()
+        require("conform").format({ async = true })
+      end,
+      mode = "",
+      desc = "Format buffer",
+    },
+  },
+  opts = {
+    formatters_by_ft = {
+      lua = { "stylua" },
+      python = { "isort", "black" },
+      javascript = { "prettierd" },
+      typescript = { "deno", "prettierd" },
+      tsx = { "deno", "prettierd" }
+    },
+    default_format_opts = {
+      lsp_format = "fallback",
+    },
+    format_on_save = { timeout_ms = 1000 },
+    formatters = {
+      shfmt = {
+        prepend_args = { "-i", "2" },
+      },
+    },
+  },
+  init = function()
+    vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+  end,
+}
